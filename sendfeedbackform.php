b97f875dde0ddee5a30b4ec10c2f49a9fe89ce98
unknown <sohampatel028@gmail.com>
2018-05-24 09:59:58 +0530

    Initial commit

---
+++ sendfeedbackform.php
@@ -0,0 +1,34 @@
+<?php
+if( isset($_POST['input-22']) && isset($_POST['input-23']) && isset($_POST['input-24']) && isset($_POST['m'])){
+	$n = $_POST['input-22']; // HINT: use preg_replace() to filter the data
+	$e = $_POST['input-23'];
+    $s = $_POST['input-24'];
+	$m = nl2br($_POST['m']);
+
+    require('phpmailer/src/SMTP.php');
+    require('phpmailer/src/PHPMailer.php');
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    
+    $mail->Host="smtp.gmail.com";
+    $mail->IsSMTP();
+    $mail->SMTPAuth=true;
+    $mail->Username="sohampatelforapple@gmail.com";
+    $mail->Password="MAIL PURPOSE";
+
+    $mail->SMTPSecure="ssl";
+    $mail->Port=465;
+
+    $mail->SetFrom('name@yourdomain.com', 'Web App');
+    $mail->Subject = $s;
+    $mail->MsgHTML($m);
+    $mail->AddAddress($e, $n);
+
+    
+    if(!$mail->Send()) {
+        echo "The server failed to send the message. Please try again later.";     }
+    else {
+        echo "success";
+     }
+
+}
+?>
